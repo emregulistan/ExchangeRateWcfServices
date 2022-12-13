81cfe8e5f38057d7d520ae395ef46552ea967bc2
emregulistan <emregulistan1@gmail.com>
2022-12-14 01:19:28 +0300

    the project with an example of wcf services has been completed

---
+++ ExchangeRate/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExchangeRate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (KurService.KurServiceClient serviceClient = new KurService.KurServiceClient())
+            {
+                if(TempData["Kurlar"] != null)
+                {
+                    ViewBag.Kurlar = (List<double>)TempData["Kurlar"];
+                }
+                else
+                {
+                    ViewBag.Kurlar = new List<double>();
+                }
+                var model = serviceClient.ParaBirimleriGetir().ToList();
+                return View(model);
+            }
+        }
+
+        public ActionResult KurGetir(string paraBirimi)
+        {
+            using (KurService.KurServiceClient serviceClient = new KurService.KurServiceClient())
+            {
+                var model = serviceClient.KurlariGetir(paraBirimi).ToList();
+                TempData["Kurlar"] = model;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ ExchangeRateService/IKurService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ExchangeRateService
+{
+    [ServiceContract]
+    public interface IKurService
+    {
+
+        [OperationContract]
+        List<string> ParaBirimleriGetir();
+
+        [OperationContract]
+        List<double> KurlariGetir(string kurTipi);
+
+    }
+}
